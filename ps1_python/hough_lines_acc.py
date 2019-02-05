f5d64101848800684883ead8844d5fc728938568
Ubira <ubiratan_melo@outlook.com>
2019-02-05 15:31:38 -0300

    PS1 Update

--- ps1_python/hough_lines_acc.py
+++ ps1_python/hough_lines_acc.py
@@ -2,9 +2,20 @@
 
 import cv2
 import numpy as np
+import math
 from matplotlib import pyplot as plt
 import os.path
 
-def hough_lines_acc(img, rho_res=1, thetas=np.arange(-90,90,1)):
-    height, width, depth = img.shape
-    
+def hough_lines_acc(img, rho_res=1, thetas=np.arange(0,180,1)):
+    height, width = img.shape
+    rhos = np.arange(-200,199,rho_res)
+    H = np.zeros((len(rhos),len(thetas)),dtype=np.uint8)
+    for c in range(0,height-1):
+        for r in range(0,width-1):
+            if img[r,c] == 255:
+                for k in range(0,179):
+                    thetas[k] = math.atan2(r,c)
+                    rhos[k] = c*math.cos(thetas[k]) + r*math.sin(thetas[k])
+                    H[rhos[k],thetas[k]] += 1;
+
+    return H, thetas, rhos
--- ps1_python/ps1.py
+++ ps1_python/ps1.py
@@ -4,8 +4,16 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import os.path
+from hough_lines_acc import *
 
 if __name__ == '__main__':
-    img = cv2.imread('input/ps1-input0.png',cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread('ps1_python/input/ps1-input0.png',cv2.IMREAD_GRAYSCALE)
     img_edges = cv2.Canny(img,0,1)
-    cv2.imwrite('output/ps1-1-a-1.png',img_edges)
+    print(img.shape)
+    cv2.imwrite('ps1_python/output/ps1-1-a-1.png',img_edges)
+
+    H, rhos, thetas = hough_lines_acc(img_edges)
+    print(len(thetas))
+    cv2.imwrite('ps1_python/output/ps1-2-a-1.png',H)
+    plt.imshow(H)
+    plt.show()
