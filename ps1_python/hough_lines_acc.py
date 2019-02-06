e8e3087a750676ca2f2f80549ed8a950f63b8abc
Ubira <ubiratan_melo@outlook.com>
2019-02-06 03:03:07 -0300

    hough_peaks implemented

--- ps1_python/hough_lines_acc.py
+++ ps1_python/hough_lines_acc.py
@@ -8,14 +8,14 @@ import os.path
 
 def hough_lines_acc(img, rho_res=1, thetas=np.arange(0,180,1)):
     height, width = img.shape
-    rhos = np.arange(-200,199,rho_res)
+    rho_size = int(math.sqrt(pow(height,2) + pow(width,2)))
+    rhos = np.arange(0,rho_size*rho_res,1)
     H = np.zeros((len(rhos),len(thetas)),dtype=np.uint8)
     for c in range(0,height-1):
         for r in range(0,width-1):
             if img[r,c] == 255:
-                for k in range(0,179):
-                    thetas[k] = math.atan2(r,c)
-                    rhos[k] = c*math.cos(thetas[k]) + r*math.sin(thetas[k])
+                for k in thetas:
+                    rhos[k] = c*math.cos(k) - r*math.sin(k)
                     H[rhos[k],thetas[k]] += 1;
 
     return H, thetas, rhos
--- ps1_python/ps1.py
+++ ps1_python/ps1.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 
 import cv2
-import numpy as np
-from matplotlib import pyplot as plt
-import os.path
 from hough_lines_acc import *
+from hough_peaks import *
 
 if __name__ == '__main__':
-    img = cv2.imread('ps1_python/input/ps1-input0.png',cv2.IMREAD_GRAYSCALE)
-    img_edges = cv2.Canny(img,0,1)
-    print(img.shape)
-    cv2.imwrite('ps1_python/output/ps1-1-a-1.png',img_edges)
+    img = cv2.imread('ps1_python/input/ps1-input0.png', cv2.IMREAD_GRAYSCALE)
+    img_edges = cv2.Canny(img, 0, 1)
+    cv2.imwrite('ps1_python/output/ps1-1-a-1.png', img_edges)
 
     H, rhos, thetas = hough_lines_acc(img_edges)
-    print(len(thetas))
-    cv2.imwrite('ps1_python/output/ps1-2-a-1.png',H)
-    plt.imshow(H)
-    plt.show()
+    cv2.imwrite('ps1_python/output/ps1-2-a-1.png', H)
+
+    peaks = hough_peaks(H, 10)
+    P = cv2.imread('ps1_python/output/ps1-2-a-1.png')
+
+    for i in range(0, len(peaks)):
+        x, y = peaks[i]
+        P = cv2.rectangle(P, (y - 2, x - 2), (y + 2, x + 2), (0, 255, 0))
+
+    cv2.imwrite('ps1_python/output/ps1-2-b-1.png', P)
