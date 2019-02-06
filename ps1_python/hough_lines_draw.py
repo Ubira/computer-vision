9282addb9050a953a1d7ff511a81ad52c47903bb
Ubira <ubiratan_melo@outlook.com>
2019-02-06 18:47:20 -0300

    PS1 Upadate

--- ps1_python/hough_lines_draw.py
+++ ps1_python/hough_lines_draw.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 
+import cv2
 import numpy as np
-import math
 
 
-def hough_lines_draw(img, img_name, peaks, theta, rho):
-    
-    return
+def hough_lines_draw(img, img_name, peaks, thetas, rhos):
+    for r in range(0, peaks.shape[0]):
+        theta = thetas[peaks[r, 1]]
+        rho = rhos[peaks[r, 0]]
+        a = np.cos(np.deg2rad(theta))
+        b = np.sin(np.deg2rad(theta))
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    name = 'ps1_python/output/' + img_name
+    cv2.imwrite(name, img)
--- ps1_python/ps1.py
+++ ps1_python/ps1.py
@@ -5,6 +5,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from hough_lines_acc import *
 from hough_peaks import *
+from hough_lines_draw import *
 
 if __name__ == '__main__':
     img = cv2.imread('ps1_python/input/ps1-input0.png', cv2.IMREAD_GRAYSCALE)
@@ -16,10 +17,12 @@ if __name__ == '__main__':
 
     peaks = hough_peaks(H, 10)
     P = cv2.imread('ps1_python/output/ps1-2-a-1.png')
-    print(peaks)
 
     for i in range(0, len(peaks)):
         x, y = peaks[i]
         P = cv2.rectangle(P, (y - 2, x - 2), (y + 2, x + 2), (0, 255, 0))
 
     cv2.imwrite('ps1_python/output/ps1-2-b-1.png', P)
+
+    img_color = cv2.imread('ps1_python/input/ps1-input0.png', cv2.IMREAD_COLOR)
+    hough_lines_draw(img_color, 'ps1-2-c-1.png', peaks, thetas, rhos)
